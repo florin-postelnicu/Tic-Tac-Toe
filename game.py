84387b9a6f001e5501cc01ecb6b6b51c1803a920
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-01-31 12:24:28 -0700

    Add files via upload

--- game.py
+++ game.py
@@ -1,47 +1,44 @@
+
+
 import pygame
 from grid import Grid
+
 import os
+os.environ['SDL_VIDEO_WINDOW_POS'] = '400,100'
 
 
-os.environ['SDL_VIDEO_WINDOW_POS'] = '400, 100' # Establish the upper
-                            # corner window's position to display on the computer's
-                            # screen
-surface = pygame.display.set_mode((600, 600)) # set the size of the
-                            # Tic- Tac - Toe board
-pygame.display.set_caption('Tic-Tac-Toe') # Set the name of the board
+surface = pygame.display.set_mode((600,600))
+pygame.display.set_caption('Tic-tac-toe')
 
-grid = Grid() # create an instance of the class Grid
+grid = Grid()
 
 
-
-
-running = True # set the running variable to start the loop
-
-player = 'x'
+running = True
+player = "X"
 
 while running:
-    for event in pygame.event.get(): # search in the pygame.event for QUIT.
-                             # to make it possible to close the window
-        if event.type == pygame.QUIT: # if click on the close button,
-               #  end the program
-               running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and not grid.game_over:
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
-                # print(pos[0]//200, pos[1]//200)
-                grid.get_mouse(pos[0]//200, pos[1]//200, player)
+                grid.get_mouse(pos[0] // 200, pos[1] // 200, player)
                 if grid.switch_player:
-
-                    if player == 'x':
-                        player = 'o'
+                    if player == "X":
+                        player = "O"
                     else:
-                        player = 'x'
+                        player = "X"
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and grid.game_over:
+                grid.clear_grid()
+                grid.game_over = False
+            elif event.key == pygame.K_ESCAPE:
+                running = False
 
-                grid.print_grid()
 
+    surface.fill((0,0,0))
 
-    surface.fill((0, 0, 0)) # fill the surface with color black
-    grid.draw(surface)   # draw the grid on the board using draw method
-    # of the class Grid
-    pygame.display.flip() # update the window
+    grid.draw(surface)
+
+    pygame.display.flip()
