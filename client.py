85756312dc665d63f0c1f6e47b7f432222d623f1
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-02-05 14:32:18 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -12,6 +12,23 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = '400,100'
 surface = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Tic-tac-toe')
 
+import threading
+def create_thread(target):
+    thread = threading.Thread(target=target)
+    thread.daemon = True
+    thread.start()
+import socket
+HOST = '127.0.0.1'
+PORT = 65432
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.connect((HOST, PORT))
+
+def receive_data():
+    pass
+
+create_thread(receive_data)
 grid = Grid()
 
 
