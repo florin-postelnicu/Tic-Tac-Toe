51c28f57131733de906ce6fcac6c8843357ea92e
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-02-09 07:47:08 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -6,27 +6,35 @@ import pygame
 from grid import Grid
 
 import os
-os.environ['SDL_VIDEO_WINDOW_POS'] = '400,100'
+os.environ['SDL_VIDEO_WINDOW_POS'] = '850,100'
 
 
 surface = pygame.display.set_mode((600,600))
-pygame.display.set_caption('Tic-tac-toe')
+pygame.display.set_caption('Tic-tac-toe Client')
 
 import threading
+
+
 def create_thread(target):
     thread = threading.Thread(target=target)
     thread.daemon = True
     thread.start()
+
+
 import socket
 HOST = '127.0.0.1'
 PORT = 65432
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 sock.connect((HOST, PORT))
 
+
 def receive_data():
-    pass
+    while True:
+        data = sock.recv(1024).decode()
+        print(data)
+
+
 
 create_thread(receive_data)
 grid = Grid()
@@ -42,7 +50,13 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN and not grid.game_over:
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
-                grid.get_mouse(pos[0] // 200, pos[1] // 200, player)
+                # Create dat to send servere
+                cellX, cellY = pos[0] // 200, pos[1] // 200
+                grid.get_mouse(cellX,cellY, player)
+                send_data = '{}-{}'.format(cellX, cellY).encode()
+                sock.send(send_data) # THAT'S HOW CLIENT SENDS DATA
+
+
                 if grid.switch_player:
                     if player == "X":
                         player = "O"
@@ -54,7 +68,6 @@ while running:
                 grid.game_over = False
             elif event.key == pygame.K_ESCAPE:
                 running = False
-
 
     surface.fill((0,0,0))
 
